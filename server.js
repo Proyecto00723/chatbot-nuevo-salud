611ee575b45c13e8bb9fc9bf254b763e616a11eb
Proyecto00723 <gustavotellov27@gmail.com>
2025-07-16 12:35:17 -0500

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
-
+ 
 // 🟢 Cargar variables desde el archivo .env
 dotenv.config();
 
